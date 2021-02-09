f24a1ebcca92e6d70881d7673fae3fe30b83cceb
StackChan <csk3121913199@gmail.com>
2021-02-09 17:56:03 +0800

    shiyan

---
+++ cmake-build-debug/CMakeFiles/shiyan.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shiyan.dir/main.cpp.obj"
+  "libshiyan.dll.a"
+  "shiyan.exe"
+  "shiyan.exe.manifest"
+  "shiyan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shiyan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
